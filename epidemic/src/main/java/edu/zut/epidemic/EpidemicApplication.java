8b6968f3f54b559a56f417aa1d06ae39e4b64db3
roydonGuo <3133010060@qq.com>
2022-09-26 20:00:23 +0800

    启动类

--- epidemic/src/main/java/edu/zut/epidemic/EpidemicApplication.java
+++ epidemic/src/main/java/edu/zut/epidemic/EpidemicApplication.java
@@ -1,13 +1,16 @@
 package edu.zut.epidemic;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class EpidemicApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EpidemicApplication.class, args);
+        log.info("项目启动中...");
     }
 
 }
