b8f863ed51a5c9d962a85f708245435a65f39f41
roydonGuo <3133010060@qq.com>
2022-09-28 19:07:25 +0800

    全局数据传输格式

--- epidemic/src/main/java/edu/zut/epidemic/common/Result.java
+++ epidemic/src/main/java/edu/zut/epidemic/common/Result.java
@@ -1,6 +1,9 @@
 package edu.zut.epidemic.common;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -9,9 +12,11 @@ import java.util.Map;
  * @param <T>
  */
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Result<T> {
 
-    private Integer code; //编码：1成功，0和其它数字为失败
+    private String code; //编码：1成功，0和其它数字为失败
 
     private String msg; //错误信息
 
@@ -19,20 +24,38 @@ public class Result<T> {
 
     private Map map = new HashMap(); //动态数据
 
-    public static <T> Result<T> success(T object) {
-        Result<T> r = new Result<T>();
-        r.data = object;
-        r.code = 1;
-        return r;
+    public static Result success() {
+        return new Result(Constants.CODE_200, "", null,null);
     }
 
-    public static <T> Result<T> error(String msg) {
-        Result r = new Result();
-        r.msg = msg;
-        r.code = 0;
-        return r;
+    public static <T> Result success(T data) {
+        return new Result(Constants.CODE_200, "", data,null);
     }
 
+    public static Result error(String code, String msg) {
+        return new Result(code, msg, null,null);
+    }
+
+    public static Result error() {
+        return new Result(Constants.CODE_500, "系统错误", null,null);
+    }
+
+
+
+//    public static <T> Result<T> success(T object) {
+//        Result<T> r = new Result<T>();
+//        r.data = object;
+//        r.code = 1;
+//        return r;
+//    }
+//
+//    public static <T> Result<T> error(String msg) {
+//        Result r = new Result();
+//        r.msg = msg;
+//        r.code = 0;
+//        return r;
+//    }
+//
     public Result<T> add(String key, Object value) {
         this.map.put(key, value);
         return this;
