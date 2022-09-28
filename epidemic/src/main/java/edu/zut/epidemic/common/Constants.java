4d1ab02760e2f818bac2def3de3a7e57cfb6f282
roydonGuo <3133010060@qq.com>
2022-09-28 19:06:54 +0800

    响应码

---
+++ epidemic/src/main/java/edu/zut/epidemic/common/Constants.java
@@ -0,0 +1,16 @@
+package edu.zut.epidemic.common;
+
+/**
+ * Created by Intellij IDEA
+ * Author: yi cheng
+ * Date: 2022/9/28
+ * Time: 18:51
+ **/
+public class Constants {
+
+    public static final String CODE_200 = "200"; //成功
+    public static final String CODE_401 = "401";  // 权限不足
+    public static final String CODE_400 = "400";  // 参数错误
+    public static final String CODE_500 = "500";// 系统错误
+    public static final String CODE_600 = "600"; // 其他业务异常
+}
