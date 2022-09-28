e610569c9dc04ad4800ae74aa531b44ba1b6ad6c
roydonGuo <3133010060@qq.com>
2022-09-28 11:33:58 +0800

    小蓝鸟鸟

---
+++ epidemic/src/main/java/edu/zut/epidemic/mapper/ResidentMapper.java
@@ -0,0 +1,16 @@
+package edu.zut.epidemic.mapper;
+
+import edu.zut.epidemic.entity.Resident;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author roydon
+ * @since 2022-09-28
+ */
+public interface ResidentMapper extends BaseMapper<Resident> {
+
+}
