68fd27e5b9f2f2ec71f67a90d49750bc988f4f32
roydonGuo <3133010060@qq.com>
2022-09-30 19:19:15 +0800

    doc

--- doc/a_db_epc.sql
+++ doc/a_db_epc.sql
@@ -11,11 +11,33 @@
  Target Server Version : 80023
  File Encoding         : 65001
 
- Date: 28/09/2022 14:11:28
+ Date: 30/09/2022 19:18:18
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for sys_file
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_file`;
+CREATE TABLE `sys_file`  (
+  `id` int(0) NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '文件名称',
+  `type` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '文件类型',
+  `size` bigint(0) NULL DEFAULT NULL COMMENT '文件大小(kb)',
+  `url` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '下载链接',
+  `md5` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '文件md5',
+  `is_delete` tinyint(1) NULL DEFAULT 0 COMMENT '是否删除',
+  `enable` tinyint(1) NULL DEFAULT 1 COMMENT '是否禁用链接',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 55 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of sys_file
+-- ----------------------------
+INSERT INTO `sys_file` VALUES (23, NULL, NULL, NULL, NULL, NULL, 1, 1);
+INSERT INTO `sys_file` VALUES (55, 'avator.jpg', 'jpg', 20, 'http://localhost:8080/file/241c55a1741d44ba9f219d4a2e4f0034.jpg', 'e828349cf573b04b32fa07f209ee1ee1', 0, 1);
 
 -- ----------------------------
 -- Table structure for sys_resident
@@ -27,30 +49,22 @@ CREATE TABLE `sys_resident`  (
   `password` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '密码',
   `nickname` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '昵称',
   `phone` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '电话',
-  `id_number` varchar(18) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '身份证',
+  `id_number` varchar(18) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '身份证',
   `email` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '邮箱',
   `address` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '地址',
   `sex` varchar(2) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '性别',
+  `age` int(0) NULL DEFAULT NULL COMMENT 'age',
   `tenant` int(0) NULL DEFAULT NULL COMMENT '是否为租客：1：租客，0：非租客',
   `update_time` datetime(0) NULL DEFAULT NULL COMMENT '更新时间',
   `create_time` datetime(0) NULL DEFAULT CURRENT_TIMESTAMP(0) COMMENT '创建时间',
   `avatar_url` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '头像',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 10017 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of sys_resident
 -- ----------------------------
-INSERT INTO `sys_resident` VALUES (10001, 'zhangsan1', '123456', '张三', '18203458965', '414148596878985689', NULL, NULL, '女', 0, '2022-09-28 11:26:32', '2022-09-28 11:26:28', NULL);
-INSERT INTO `sys_resident` VALUES (10002, NULL, '1', NULL, '1', '1', NULL, NULL, NULL, NULL, NULL, NULL, NULL);
-INSERT INTO `sys_resident` VALUES (10003, 'zhangsan2', '123456', '张三', '18203458965', '414148596878985689', NULL, NULL, '女', 0, '2022-09-28 11:26:32', '2022-09-28 11:26:28', NULL);
-INSERT INTO `sys_resident` VALUES (10004, 'zhangsan2', '123456', '张三', '18203458965', '414148596878985689', NULL, NULL, '女', 0, '2022-09-28 11:26:32', '2022-09-28 11:26:28', NULL);
-INSERT INTO `sys_resident` VALUES (10005, 'zhangsan2', '123456', '张三', '18203458965', '414148596878985689', NULL, NULL, '女', 0, '2022-09-28 11:26:32', '2022-09-28 11:26:28', NULL);
-INSERT INTO `sys_resident` VALUES (10006, NULL, NULL, '2', '2', '2', '12', '2', '2', 11, NULL, '2022-09-28 13:52:07', NULL);
-INSERT INTO `sys_resident` VALUES (10007, 'zhangsan2', '123456', '张三', '18203458965', '414148596878985689', NULL, NULL, '女', 0, '2022-09-28 11:26:32', '2022-09-28 11:26:28', NULL);
-INSERT INTO `sys_resident` VALUES (10008, 'zhangsan2', '123456', '张三', '18203458965', '414148596878985689', NULL, NULL, '女', 0, '2022-09-28 11:26:32', '2022-09-28 11:26:28', NULL);
-INSERT INTO `sys_resident` VALUES (10009, 'zhangsan6', '123456', '张三', '18203458965', '414148596878985689', NULL, NULL, '女', 0, '2022-09-28 11:26:32', '2022-09-28 11:26:28', NULL);
-INSERT INTO `sys_resident` VALUES (10010, 'zhangsan6', '123456', '张三', '18203458965', '414148596878985689', NULL, NULL, NULL, 0, NULL, '2022-09-28 13:53:48', NULL);
-INSERT INTO `sys_resident` VALUES (10011, 'lisi', 'e10adc3949ba59abbe56e057f20f883e', 'lisi', '18203458965', '414148596878985689', NULL, NULL, NULL, 0, NULL, '2022-09-28 13:53:52', NULL);
+INSERT INTO `sys_resident` VALUES (10021, 'zhangsan', 'e10adc3949ba59abbe56e057f20f883e', NULL, '18203458965', NULL, NULL, NULL, NULL, NULL, NULL, '2022-09-29 21:08:39', '2022-09-29 21:08:38', NULL);
+INSERT INTO `sys_resident` VALUES (10022, 'lisi', 'dc3a8f1670d65bea69b7b65048a0ac40', NULL, '18203458965', NULL, NULL, NULL, NULL, NULL, NULL, '2022-09-29 21:09:18', '2022-09-29 21:09:18', NULL);
 
 SET FOREIGN_KEY_CHECKS = 1;
