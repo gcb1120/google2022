b41bb7518586225d75f18485891f136a716fa66c
roydonGuo <3133010060@qq.com>
2022-09-30 18:41:05 +0800

    FileMapper

---
+++ epidemic/src/main/java/edu/zut/epidemic/mapper/FileMapper.java
@@ -0,0 +1,13 @@
+package edu.zut.epidemic.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import edu.zut.epidemic.entity.Files;
+
+/**
+ * Created by Intellij IDEA
+ * Author: yi cheng
+ * Date: 2022/9/29
+ * Time: 21:14
+ **/
+public interface FileMapper extends BaseMapper<Files> {
+}
